e6dd97faa08e6b4cbc218dc1c7976e8600e73aa8
sumit-cse-xyz <sumitkumar4204200@gmail.com>
2026-09-09 10:43:28 +0530

    Add program to join two words from input
    Implemented a program to join two input words.

---
+++ 02_join_words.py
@@ -0,0 +1,20 @@
+# Question:
+# Write a program to join two words given as input.
+
+# Input:
+# Two words
+
+# Example Input:
+# Hello
+# World
+
+# Output:
+# Print the two words joined together
+
+# Example Output:
+# HelloWorld
+
+word1 = input()
+word2 = input()
+
+print(word1 + word2)
