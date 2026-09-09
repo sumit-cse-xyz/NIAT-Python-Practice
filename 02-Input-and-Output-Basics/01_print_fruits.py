2016cba9f67adee5a1096ad7e0920b61c2a7e0c6
sumit-cse-xyz <sumitkumar4204200@gmail.com>
2026-09-09 10:39:21 +0530

    Implement fruit input and output program
    Added a program to take and print two fruit names.

---
+++ 02-Input-and-Output-Basics/01_print_fruits.py
@@ -0,0 +1,16 @@
+# Question:
+# Write a program to take two fruit names as input and print them.
+
+# Input:
+#Apple
+#Banana
+
+# Output:
+# Print both fruit names
+
+
+fruit1 = input()
+fruit2 = input()
+
+print(fruit1)
+print(fruit2)
