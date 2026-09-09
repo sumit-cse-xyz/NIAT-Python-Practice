dee8b897eb4d0371612054ae2bd5935616b359b3
sumit-cse-xyz <sumitkumar4204200@gmail.com>
2026-09-09 13:41:59 +0530

    Create 08_print_first_letter.py

---
+++ 02-Input-and-Output-Basics/08_print_first_letter.py
@@ -0,0 +1,13 @@
+## Question
+
+#Write a Python program to take a string as input and print its first character.
+
+## Answer
+
+#input=(sumit)
+
+
+play = input()
+first_letter = play[0]
+print(first_letter)
+
