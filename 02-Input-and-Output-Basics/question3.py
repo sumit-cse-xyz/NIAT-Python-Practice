132f235d073fe86c7bd606282bd9b49539f29f86
sumit-cse-xyz <sumitkumar4204200@gmail.com>
2026-09-09 13:23:32 +0530

    Add program to print two input words
    Implement a program to take two words as input and print them separated by a space.

---
+++ 02-Input-and-Output-Basics/question3.py
@@ -0,0 +1,13 @@
+#Question: Write a Python program to take two words as input and print them separated by a space.
+#input: 
+#harry 
+#porter    
+
+
+
+#solution:
+
+a = input()
+b = input()
+
+print(a + " " + b)
