512ec35255732294252c8232d9ebafdaf8992fa2
sumit-cse-xyz <sumitkumar4204200@gmail.com>
2026-09-09 14:02:28 +0530

    Add star repetition program
    Implement a program to print the first letter of a word followed by asterisks.

---
+++ 02-Input-and-Output-Basics/12_star_repetation.py
@@ -0,0 +1,13 @@
+#question
+Write a program that prints the first letter of the given word and stars(*) instead of other letters?
+
+input=python
+
+
+a=input()
+b=len(a)-1
+c=b * "*"
+print(a[0]+c)
+
+
+output=p*****
