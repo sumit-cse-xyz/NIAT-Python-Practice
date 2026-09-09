2a5952370d7a471442dbc6045fa77684382c1eb4
sumit-cse-xyz <sumitkumar4204200@gmail.com>
2026-09-09 13:48:22 +0530

    Add reversing_digit.py to reverse number digits
    Added a Python program to reverse the digits of a number using string slicing.

---
+++ 02-Input-and-Output-Basics/09_reversing_digit.py
@@ -0,0 +1,10 @@
+## Question
+
+Write a Python program to take a number as input and print its digits in reverse order using string slicing.
+
+## Answer
+input= sumit
+
+n = input()
+print(n[::-1])
+
