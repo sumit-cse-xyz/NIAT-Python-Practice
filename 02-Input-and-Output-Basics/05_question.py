f689b58f7f82b6e6ed4e3c5a880303b3be5f75e0
sumit-cse-xyz <sumitkumar4204200@gmail.com>
2026-09-09 13:29:14 +0530

    Create 05_question.py for printing '*' character
    Add a Python program to print '*' twice with spaces.

---
+++ 02-Input-and-Output-Basics/05_question.py
@@ -0,0 +1,12 @@
+
+## Question
+
+Write a Python program to print the `*` character twice on separate lines, with a space after each `*`.
+
+## Answer
+
+a = "*"
+
+print((a + " ") * 2)
+print((a + " ") * 2)
+
