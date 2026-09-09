5a3460211d5d01b29cb1e750000b25d3c5ba3015
sumit-cse-xyz <sumitkumar4204200@gmail.com>
2026-09-09 13:37:13 +0530

    Add string repetition program in 07_string_repetation.py
    Added a Python program that takes a string input and prints it three times with spaces.

---
+++ 02-Input-and-Output-Basics/07_string_repetation.py
@@ -0,0 +1,12 @@
+## Question
+
+#Write a Python program to take a string as input and print it three times, with a space after each repetition.
+
+## Answer
+#input=(sumit)
+
+
+
+a = input()
+print((a + " ") * 3)
+
