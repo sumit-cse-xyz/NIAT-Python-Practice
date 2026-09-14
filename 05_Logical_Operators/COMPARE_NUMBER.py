b7e394f505148d75d4edb45f73820c53a857207e
sumit_codez <sumitkumar4204200@gmail.com>
2026-09-14 21:00:19 +0530

    Add COMPARE_NUMBER.py for number comparison logic
    Implement a program to compare two numbers based on positivity and a threshold.

---
+++ 05_Logical_Operators/COMPARE_NUMBER.py
@@ -0,0 +1,6 @@
+question. Write a program that reads two numbers A and B, and checks if both A and B are positive numbers or both A and B are less than 70.
+
+
+a=int(input())
+b=int(input())
+print(a>b or (a>35 and b>35))
