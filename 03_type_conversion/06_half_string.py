aea00df468256da797a70e2cf9c18cfd7367122e
sumit_codez <sumitkumar4204200@gmail.com>
2026-09-14 11:21:51 +0530

    Add half string functionality
    Implement a script to print the first half of a string with an even number of characters.

---
+++ 03_type_conversion/06_half_string.py
@@ -0,0 +1,29 @@
+# Half String
+
+# Problem Statement:
+
+# Given a string with an even number of characters, print the first half.
+
+#
+
+# Input:
+
+# A string.
+
+#
+
+# Output:
+
+# Print the first half of the string.
+
+#
+
+# Example:
+
+# Input: Python
+
+# Output: Pyt
+
+a = input()
+n = len(a)
+print(a[:n // 2])
