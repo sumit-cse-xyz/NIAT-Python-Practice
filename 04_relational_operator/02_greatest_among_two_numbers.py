b3fcdb28c0d31c4210ff0687dab0759835e2a4d8
sumit_codez <sumitkumar4204200@gmail.com>
2026-09-14 11:35:07 +0530

    Add program to determine the greater of two numbers
    Implement a program to find the greatest among two numbers.

---
+++ 04_relational_operator/02_greatest_among_two_numbers.py
@@ -0,0 +1,27 @@
+```python
+# Greatest among Two Numbers
+# Problem Statement:
+# Given two numbers, check which number is greater.
+#
+# Input:
+# Two integers.
+#
+# Output:
+# Print the greater number.
+#
+# Example:
+# Input:
+# 15
+# 20
+#
+# Output:
+# 20
+
+a = int(input())
+b = int(input())
+
+if a > b:
+    print(a)
+else:
+    print(b)
+```
