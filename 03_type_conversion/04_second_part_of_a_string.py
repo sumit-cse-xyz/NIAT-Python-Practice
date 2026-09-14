97b5a0e2971ca06cec70276a2444af9bc3b16bf2
sumit_codez <sumitkumar4204200@gmail.com>
2026-09-14 11:20:42 +0530

    Add script to print second half of a string

---
+++ 03_type_conversion/04_second_part_of_a_string.py
@@ -0,0 +1,29 @@
+# Second Part of a String
+
+# Problem Statement:
+
+# Given a string with an even number of characters, print the second half.
+
+#
+
+# Input:
+
+# A string.
+
+#
+
+# Output:
+
+# Print the second half of the string.
+
+#
+
+# Example:
+
+# Input: Python
+
+# Output: hon
+
+a = input()
+n = len(a)
+print(a[n // 2:])
