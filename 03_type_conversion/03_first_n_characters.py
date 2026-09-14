0f29c9ac53dd80cd6509068a2e04eb6435b6a2ad
sumit_codez <sumitkumar4204200@gmail.com>
2026-09-14 11:20:11 +0530

    Add script to print first N characters of a string
    Implemented a script to print the first N characters of a given string based on user input.

---
+++ 03_type_conversion/03_first_n_characters.py
@@ -0,0 +1,39 @@
+# First N Characters
+
+# Problem Statement:
+
+# Given a string and a number N, print the first N characters of the string.
+
+#
+
+# Input:
+
+# First line contains a string.
+
+# Second line contains an integer N.
+
+#
+
+# Output:
+
+# Print the first N characters.
+
+#
+
+# Example:
+
+# Input:
+
+# Programming
+
+# 4
+
+#
+
+# Output:
+
+# Prog
+
+a = input()
+n = int(input())
+print(a[:n])
