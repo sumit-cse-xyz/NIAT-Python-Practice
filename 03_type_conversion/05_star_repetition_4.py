4b50506ad93e43b258b9d94314ba91dc520b0f5c
sumit_codez <sumitkumar4204200@gmail.com>
2026-09-14 11:21:17 +0530

    Add star repetition program
    Implement star repetition based on user input.

---
+++ 03_type_conversion/05_star_repetition_4.py
@@ -0,0 +1,28 @@
+# Star Repetition - 4
+
+# Problem Statement:
+
+# Given a number N, print the * character N times.
+
+#
+
+# Input:
+
+# An integer N.
+
+#
+
+# Output:
+
+# Print * N times.
+
+#
+
+# Example:
+
+# Input: 4
+
+# Output: ****
+
+n = int(input())
+print("*" * n)
