9c74f1d032c9f7464fdd1245d6cb942abaf4c439
sumit_codez <sumitkumar4204200@gmail.com>
2026-09-14 22:21:37 +0530

    Add digit_zero.py to check for zero in input
    Implement a program to check for zero in a three-digit number.

---
+++ 05_Logical_Operators/digit_zero.py
@@ -0,0 +1,12 @@
+Write a program that reads a three-digit number and checks if the given number contains 0.
+
+Input:
+The input will be a single line containing a three-digit integer.
+
+Output:
+The output should be a single line containing a boolean.
+True should be printed if the given number contains 0, otherwise False should be printed.
+
+
+a=(input())
+print(int(a[0])==0 or int(a[1])==0 or int(a[2])==0)                                                     
