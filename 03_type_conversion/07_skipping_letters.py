82cdacf4bc2e358d00ff0779b6f0a185fd0c6bd4
sumit_codez <sumitkumar4204200@gmail.com>
2026-09-14 11:22:32 +0530

    Add skipping letters functionality
    Implement a function to print characters by skipping every alternate character in a given string.

---
+++ 03_type_conversion/07_skipping_letters.py
@@ -0,0 +1,28 @@
+# Skipping Letters
+
+# Problem Statement:
+
+# Given a string, print characters by skipping every alternate character.
+
+#
+
+# Input:
+
+# A string.
+
+#
+
+# Output:
+
+# Print characters at alternate positions.
+
+#
+
+# Example:
+
+# Input: Python
+
+# Output: Pto
+
+a = input()
+print(a[::2])
