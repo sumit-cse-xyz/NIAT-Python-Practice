adc4542b7ae672b76efb58297a747fe0a0d85ba6
sumit_codez <sumitkumar4204200@gmail.com>
2026-09-14 21:57:23 +0530

    Add program to check for 'woman' in input strings
    Implement a program to check if any input string equals 'woman'.

---
+++ 05_Logical_Operators/Women.py
@@ -0,0 +1,19 @@
+Write a program that reads three strings A, B, and C and checks if one of the given strings is equal to woman.
+
+Input:
+The first line of input contains a string A.
+The second line of input contains a string B.
+The third line of input contains a string C.
+
+Output:
+The output should be a single line containing a boolean.
+True should be printed if one of the given strings is equal to woman, otherwise False should be printed.
+
+
+  
+
+a=input()
+b=input()
+c=input()
+d="woman"
+print(a==d or b==d or c==d)
