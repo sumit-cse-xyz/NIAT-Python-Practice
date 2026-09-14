c0b8b0ee8430f825e7a75251aab544a24c9e6d67
sumit_codez <sumitkumar4204200@gmail.com>
2026-09-14 11:19:06 +0530

    Add script for printing first three characters of string
    Implemented a script to print the first three characters of a given string.

---
+++ 03_type_conversion/01_first_three_characters.py
@@ -0,0 +1,28 @@
+# First Three Characters
+
+# Problem Statement:
+
+# Given a string as input, print the first three characters of the string.
+
+#
+
+# Input:
+
+# A string.
+
+#
+
+# Output:
+
+# Print the first three characters.
+
+#
+
+# Example:
+
+# Input: Python
+
+# Output: Pyt
+
+a = input()
+print(a[:3])
