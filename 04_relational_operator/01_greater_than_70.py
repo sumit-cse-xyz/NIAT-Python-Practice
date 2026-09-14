994f81d78c523cda8bbf89e637492021ea139cd0
sumit_codez <sumitkumar4204200@gmail.com>
2026-09-14 11:34:28 +0530

    Add script to check if number is greater than 70
    This script checks if a given integer is greater than 70 and prints the result.

---
+++ 04_relational_operator/01_greater_than_70.py
@@ -0,0 +1,18 @@
+```python
+# Greater than 70
+# Problem Statement:
+# Given a number, check whether it is greater than 70.
+#
+# Input:
+# An integer.
+#
+# Output:
+# Print True if the number is greater than 70, otherwise print False.
+#
+# Example:
+# Input: 85
+# Output: True
+
+a = int(input())
+print(a > 70)
+```
