6ded59298266819a4e9c90fe3ee7f20dd9278029
sumit_codez <sumitkumar4204200@gmail.com>
2026-09-17 09:43:39 +0530

    Add temperature check for walk in go-to-walk.py
    Implement temperature check for walk eligibility.

---
+++ Conditional_Statements.py/go-to-walk.py
@@ -0,0 +1,17 @@
+Question:
+Write a program that reads a temperature and checks if the given temperature is between 15 and 40.
+Print Can go for a walk if the given temperature is between 15 and 40, otherwise print Cannot go for a walk.
+
+Input:
+The input will be a single line containing an integer.
+
+Output:
+The output should be a single line containing a string.
+Can go for a walk should be printed if the temperature is between 15 and 40, otherwise Cannot go for a walk should be printed.
+
+
+
+if 15<int(input())<40:
+    print("Can go for a walk")
+else:
+    print("Cannot go for a walk")                                                                          
